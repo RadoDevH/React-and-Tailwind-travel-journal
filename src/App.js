a4a87fb189cfc6324c83440cab255a4c17b6bcc2
Radek Holubowski <radekh8787@gmail.com>
2022-01-17 22:50:10 +0000

    add styling to Navbar.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+//import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="flex flex-col w-card h-card bg-blue-500 rounded-2xl overflow-hidden">
+    <div className="flex flex-col w-card h-card bg-white rounded-2xl overflow-hidden">
       <Navbar />
       <Header />
-      <Footer />
     </div>
   );
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-row justify-center items-center  w-full h-[80px] bg-[#f55a5a]">
+      <div className="flex justify-center w-60">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <h3 className="text-white capitalize pl-2">my travel journal</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
