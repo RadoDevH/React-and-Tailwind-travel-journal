f95e415ed2ec9000aee3d79545a9117f6fe08665
Radek Holubowski <radekh8787@gmail.com>
2022-01-17 21:52:13 +0000

    add component directory with 3 components files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!!!</h1>
+    <div className="flex flex-col w-card h-card bg-blue-500 rounded-2xl overflow-hidden">
+      <Navbar />
+      <Header />
+      <Footer />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        card: "550px",
+      },
+      height: {
+        card: "870px",
+      },
+    },
   },
   plugins: [],
 };
