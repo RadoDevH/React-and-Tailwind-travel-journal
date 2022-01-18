2cd35ad21c134357906dd73033b073b334e9697f
Radek Holubowski <radekh8787@gmail.com>
2022-01-18 17:41:48 +0000

    changed header.js file for card.js and add styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
+import Card from "./components/Card";
+import { data } from "./data";
 //import Footer from "./components/Footer";
 
 const App = () => {
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
   return (
     <div className="flex flex-col w-card h-card bg-white rounded-2xl overflow-hidden">
       <Navbar />
-      <Header />
+      <section className="h-full px-5 pt-5">{cards}</section>
     </div>
   );
 };
---
+++ src/components/Card.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Card = ({
+  image,
+  title,
+  location,
+  googleMapsUrl,
+  startDate,
+  endDate,
+  description,
+}) => {
+  return (
+    <div className="flex  mb-5 h-[200px] ">
+      <div className=" w-1/3  mr-2">
+        <img
+          className="rounded-lg object-cover h-full  w-full"
+          src={image}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col justify-between flex-1 ">
+        <div>
+          <div className="flex ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 text-[#f55a5a]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            <span className="uppercase mr-2">{location}</span>
+            <a
+              href={googleMapsUrl}
+              className="font-light underline decoration-0"
+            >
+              View on Google Maps
+            </a>
+          </div>
+          <h2 className="mb-3 font-bold text-2xl">{title}</h2>
+          <p className="font-semibold mt-10">
+            <span>{startDate}</span> - <span>{endDate}</span>
+            <p className="text-sm font-light">{description}</p>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
